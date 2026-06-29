00619d3a86a571d4de7dbb4bd6c23f265ac1b5ba
maverikkk-cmyk <maverikkk@gmail.com>
2026-06-29 15:29:48 +0530

    Create index.html

---
+++ index.js
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>ShadowX Login</title>
+</head>
+<body>
+
+<h2>Login System</h2>
+
+<input id="username" placeholder="username" />
+<input id="password" placeholder="password" />
+<button onclick="login()">Login</button>
+
+<p id="res"></p>
+
+<script>
+const API = "https://shadowxcoding-999.onrender.com";
+
+async function login() {
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  const res = await fetch(API + "/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ username, password })
+  });
+
+  const data = await res.json();
+  document.getElementById("res").innerText =
+    data.token ? "Login OK 🔥" : "Fail 💀";
+}
+</script>
+
+</body>
+</html>
